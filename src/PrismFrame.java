e6136078fd9725e69152c6f2cf626c21d2cf245e
Qto777 <79415655+Qto777@users.noreply.github.com>
2021-04-06 21:26:45 +0200

    Imported libs

--- src/PrismFrame.java
+++ src/PrismFrame.java
@@ -1,5 +1,17 @@
+import java.awt.Color;
+import java.util.Random;
 
 public class PrismFrame {
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JSlider;
+import javax.swing.JTextField;
 
 	public PrismFrame() {
 		// TODO Auto-generated constructor stub
