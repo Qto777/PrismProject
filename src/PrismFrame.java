2fa766d6a28aebdef37cd905d2cf0d28763aa1ba
Qto777 <79415655+Qto777@users.noreply.github.com>
2021-04-06 22:59:51 +0200

    Some more imports

--- src/PrismFrame.java
+++ src/PrismFrame.java
@@ -1,7 +1,14 @@
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.util.Random;
 
 public class PrismFrame {
+import java.awt.Component;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JLabel;
